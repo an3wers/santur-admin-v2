0b73a88d8e8c057f3f83aa0a34a504e2beed7a11
an3wers <an3wer@yandex.ru>
2025-12-29 15:46:47 +0500

    update: use-edit-post-item - temporarily isModified flag only true

--- src/features/post/model/use-edit-post-item.ts
+++ src/features/post/model/use-edit-post-item.ts
@@ -20,13 +20,16 @@ export const usePostEditItem = ({ catId }: { catId: number }) => {
     previewImage: undefined
   })
 
-  const isModified = ref(false)
+  const isModified = ref(true)
 
-  watch(postItem, (newValue, oldValue) => {
-    if ((status.value === 'success' && oldValue.title !== '') || status.value === 'idle') {
-      isModified.value = !(isEqual(newValue, oldValue) && isEqual(newValue, originalPost))
-    }
-  })
+  // TODO: Реализовать проверку на изменения
+  // сейчас впринципе некорректно работает, так как postItem всегда указывает на один и тот же объект - newValue, oldValue всегда будут равны
+  // также доработать проверка с учетом поля previewImage, originalPost не содержит этого поля
+  // watch(postItem, (newValue, oldValue) => {
+  //   if ((status.value === 'success' && oldValue.title !== '') || status.value === 'idle') {
+  //     isModified.value = !(isEqual(newValue, oldValue) && isEqual(newValue, originalPost))
+  //   }
+  // })
 
   const status = ref<ProcessStatus>('idle')
 
@@ -63,8 +66,6 @@ export const usePostEditItem = ({ catId }: { catId: number }) => {
           previewImgUrl: res.previewImgUrl
         })
       )
-
-
 
       status.value = 'success'
     } catch (error) {
