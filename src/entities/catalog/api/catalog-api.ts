dd6e28b99aba94c506358e2a5fb2ed302bbc1637
an3wers <an3wer@yandex.ru>
2026-06-09 12:55:37 +0500

    add: api for get catalog vids

--- src/entities/catalog/api/catalog-api.ts
+++ src/entities/catalog/api/catalog-api.ts
@@ -2,6 +2,7 @@ import { useAppRequest } from '~/shared/libs/api/use-app-requests'
 import type {
   DownloadTemplateOption,
   GetCatalogItemDto,
+  GetCatalogVidsItemRes,
   GetPresetFiltersByCatalogItemRes,
   GetPresetsFilters,
   SaveNewPresetFilterItem
@@ -24,7 +25,8 @@ export const useCatalogApi = () => {
   }
 
   async function getCatalogItem(itemId: string | number) {
-    const res = await fetchWithToken<GetCatalogItemDto>(`AdminGoods/GetTntk?id=${itemId}`)
+    // const res = await fetchWithToken<GetCatalogItemDto>(`AdminGoods/GetTntk?id=${itemId}`)
+    const res = await fetchWithToken<GetCatalogItemDto>(`AdminGoods/GetCatalogItem?id=${itemId}`)
     return checkError(res).data
   }
 
@@ -48,9 +50,6 @@ export const useCatalogApi = () => {
     return checkError(res).data
   }
 
-  //  2) GET: api/admingoods/GetPresetFiltersByCatalogItem?id=100824
-  //  где id - id товарной категории
-
   async function getPresetFiltersByCatalogItem(tkId: number) {
     const res = await fetchWithToken<GetPresetFiltersByCatalogItemRes>(
       'admin/catalog/GetPresetFiltersByCatalogItem',
@@ -61,26 +60,17 @@ export const useCatalogApi = () => {
     return checkError(res).data
   }
 
-  //  3) POST:api/admingoods/SavePresetFilterForCatalogItem
-  //  в боди структура:
-  //  {
-  //      "id":100001,
-  //      "catalogItemId":100824,
-  //      "title" : "Гидры больше 500 литров",
-  //      "descr":"<p>самые огромные гидроаккумы</p>",
-  //      "shortDescr":"Очень крупные",
-  //      "presets": [{ "name":"Объем","selected":"500л;750л","minSelect":"","maxSelect":""}],
-  //      "alias":"ochkrupnyie"
-  //  }
-
-  //  поле presets - массив, т.е. например  [
-  // { "name":"Объем","selected":"500л;750л","minSelect":"","maxSelect":""},{"name":"Цена","selected":"","minSelect":"50000","MaxSelect":"400000"}
-  //  ]
-  //
   async function savePresetFilterForCatalogItem(data: SaveNewPresetFilterItem) {
     const res = await fetchWithToken<unknown>('admin/catalog/SavePresetFilterForCatalogItem', {
       method: 'POST',
       body: data
+    })
+    return checkError(res).data
+  }
+
+  async function getCatalogVids(tkId?: number) {
+    const res = await fetchWithToken<GetCatalogVidsItemRes[]>('AdminGoods/GetVids', {
+      query: { tk_id: tkId }
     })
     return checkError(res).data
   }
@@ -93,6 +83,7 @@ export const useCatalogApi = () => {
     downloadDescriptionTemplate,
     getPresetsFilters,
     getPresetFiltersByCatalogItem,
-    savePresetFilterForCatalogItem
+    savePresetFilterForCatalogItem,
+    getCatalogVids
   }
 }
--- src/entities/catalog/model/catalog-types.ts
+++ src/entities/catalog/model/catalog-types.ts
@@ -37,3 +37,29 @@ export interface CatalogItemModel {
   shortDescr: string
   imgExist: boolean
 }
+
+export interface CatalogVidsItem {
+  id: number
+  parent_id: number
+  parent_name: string
+  vid: string
+  name: string
+  num: number
+  seotitle: string
+  keywords: string
+  alias: string
+  descr: string
+  shortDescr: string
+  imgExist: boolean
+  image: {
+    id: number
+    mediaType: string
+    url: string
+  } | null
+  gimg: string
+  vids: string[]
+  isNl: boolean | null
+  isAcc: boolean | null
+  isMC: boolean | null
+  isForOwner: boolean | null
+}
